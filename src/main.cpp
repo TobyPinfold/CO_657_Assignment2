d43b84f47b3fa6d67b7e04aa20e648790dcd3d05
Toby <kapersky71@gmail.com>
2018-10-27 20:15:15 +0100

    s
    s

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,35 @@
 #include <mbed.h>
-#include <rtos.h>
-#include <mbed_events.h>
-#include <iostream>
 #include <EthernetInterface.h>
 #include <LM75B.h>
 #include <C12832.h>
+
 using namespace std;
  
 struct PACKET {
         uint16_t senderID;
         uint8_t sequenceNumber;
-        uint8_t packetOptions;        
+        uint8_t packetOptions;   
         uint16_t temperature;
         uint8_t buttonPresses;
         uint8_t checksum;
+        uint64_t packet;
 };
 
 C12832 lcd (D11, D13, D12, D7, D10);
 LM75B temperatureSensor(PTE25, PTE24);
 
-uint16_t getTemperature();
-void sendPacket(PACKET packet);
+float getTemperature();
+void sendPacket(EthernetInterface eth, PACKET &packet);
 void connectEthernet(EthernetInterface eth);
 
 int main() {
     EthernetInterface eth;
     PACKET packet;
-
     connectEthernet(eth);
-    uint16_t temperature = getTemperature();
-    float x = temperature;
-    lcd.cls();
-    lcd.printf("Temperature is %.2f C\n", x);
-    sendPacket(packet);
+    packet.senderID = 21486;
+    packet.temperature = (uint16_t)getTemperature();
+
+    sendPacket(eth, packet);
 }
 
 void connectEthernet(EthernetInterface eth) {
@@ -40,12 +37,13 @@ void connectEthernet(EthernetInterface eth) {
     eth.connect();
     lcd.locate(0,0);
     lcd.printf("IP Address is %s \n", eth.get_ip_address());
+    wait_ms(2000);
 }
 
-uint16_t getTemperature() {
-    uint16_t temperature = 0.0;
-    temperature = temperatureSensor.read();
-    return temperature;
+float getTemperature() {
+    uint16_t temperature = temperatureSensor.read();
+    uint16_t temp2 = (temperature << 8) | (temperature >> 8);
+    return temp2 / 256.0;
 }
 
 void setSequenceNumber(int) {
@@ -72,6 +70,20 @@ void generateChecksum() {
 
 }
 
-void sendPacket(PACKET packet) {
+
+
+void sendPacket(EthernetInterface eth, PACKET &packet) {
+    UDPSocket sock(&eth);
+    SocketAddress sockAddr;
     
+    lcd.cls();
+    lcd.locate(0,0);
+    uint64_t val = 0xdb12010019202000;
+
+    int status = sock.sendto("http://lora.kent.ac.uk", 1789 ,  &val , sizeof(uint64_t));
+    if(0 > status) {
+        lcd.printf("Error sending data\n %n" , status);
+    } else {
+        lcd.printf("sent?");
+    }
 }
