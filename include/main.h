06fc0c6b6b26e5ddc2a9aa2e74a0c6093256ed3f
Toby Pinfold <tp301@kent.ac.uk>
2018-10-28 20:55:08 +0000

    basics done
    basics done

---
+++ include/main.h
@@ -0,0 +1,104 @@
+#include <mbed.h>
+#include <EthernetInterface.h>
+#include <LM75B.h>
+#include <C12832.h>
+
+#define SW2_VAL 1
+#define SW3_VAL 2
+#define JOYSTICK_UP_VAL 4
+#define JOYSTICK_DOWN_VAL 8
+#define JOSYTICK_LEFT_VAL 16
+#define JOYSTICK_RIGHT_VAL 32
+#define JOYSTICK_FIRE_VAL 64
+
+C12832 lcd(D11, D13, D12, D7, D10);
+LM75B temperatureSensor(PTE25, PTE24);
+EthernetInterface eth;
+
+InterruptIn sw2(SW2);
+InterruptIn sw3(SW3);
+InterruptIn joystickUp(A2);
+InterruptIn joystickDown(A3);
+InterruptIn joystickLeft(A4);
+InterruptIn joystickRight(A5);
+InterruptIn joystickFire(D4);
+
+volatile uint8_t buttonsPressed = 0;
+volatile uint8_t sequenceNumber = 1;
+
+bool sw2Pressed = false;
+bool sw3Pressed = false;
+bool joystickUpPressed = false;
+bool joystickDownPressed = false;
+bool joystickLeftPressed = false;
+bool joystickRightPressed = false;
+bool joystickFirePressed = false;
+
+struct PACKET
+{
+    uint8_t checksum;
+    uint8_t buttonPresses;
+    uint16_t temperature;
+    uint8_t packetOptions;
+    uint8_t sequenceNumber;
+    uint16_t senderID;
+};
+
+void buttonDown();
+
+void sendTemperatureUpdate();
+
+void sendPacket(EthernetInterface eth, uint64_t packet);
+
+void setSenderID(PACKET &packet, uint16_t id);
+
+void setSequenceNumber(PACKET &packet);
+
+void setPacketOptions(PACKET &packet, bool retryFlag, bool ccittFlag, bool ackRequestFlag);
+
+void setButtonPresses(PACKET &packet);
+
+void setTemperature(PACKET &packet);
+
+void generateChecksum(PACKET &packet);
+
+void connectEthernet(EthernetInterface eth);
+
+uint64_t buildPacket(PACKET packet);
+
+void clearPressedButtons();
+
+void onSW2Pressed()
+{
+    sw2Pressed = true;
+}
+
+void onSW3Pressed()
+{
+    sw3Pressed = true;
+}
+
+void onJoystickDownPressed()
+{
+    joystickDownPressed = true;
+}
+
+void onJoystickUpPressed()
+{
+    joystickUpPressed = true;
+}
+
+void onJoystickLeftPressed()
+{
+    joystickLeftPressed = true;
+}
+
+void onJoystickRightPressed()
+{
+    joystickRightPressed = true;
+}
+
+void onJoystickFirePressed()
+{
+    joystickFirePressed = true;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,150 @@
-#include <mbed.h>
-#include <EthernetInterface.h>
-#include <LM75B.h>
-#include <C12832.h>
 
-using namespace std;
-
-struct PACKET
-{
-    uint16_t senderID;
-    uint8_t sequenceNumber;
-    uint8_t packetOptions;
-    uint16_t temperature;
-    uint8_t buttonPresses;
-    uint8_t checksum;
-    uint64_t packet;
-};
-
-C12832 lcd(D11, D13, D12, D7, D10);
-LM75B temperatureSensor(PTE25, PTE24);
-
-float getTemperature();
-void sendPacket(EthernetInterface eth, PACKET &packet);
-void connectEthernet(EthernetInterface eth);
+#include <main.h>
 
 int main()
 {
-    EthernetInterface eth;
-    PACKET packet;
     connectEthernet(eth);
-    packet.senderID = 21486;
-    packet.temperature = (uint16_t)getTemperature();
 
-    sendPacket(eth, packet);
+    sendTemperatureUpdate();
+
+    sw2.fall(&onSW2Pressed);
+    sw3.fall(&onSW3Pressed);
+    joystickDown.fall(&onJoystickDownPressed);
+    joystickUp.fall(&onJoystickUpPressed);
+    joystickLeft.fall(&onJoystickLeftPressed);
+    joystickRight.fall(&onJoystickRightPressed);
+    joystickFire.fall(&onJoystickFirePressed);
+}
+
+void sendTemperatureUpdate()
+{
+    PACKET packet;
+    setSenderID(packet, 21486);
+    setSequenceNumber(packet);
+    setPacketOptions(packet, false, false, true);
+    setTemperature(packet);
+    setButtonPresses(packet);
+    generateChecksum(packet);
+    uint64_t builtPacket = buildPacket(packet);
+    lcd.cls();
+    lcd.printf("Built %llX", builtPacket);
+    sendPacket(eth, builtPacket);
 }
 
 void connectEthernet(EthernetInterface eth)
 {
-    nsapi_error_t status = eth.connect();
+    lcd.printf("trying to connect %s \n", eth.get_ip_address());
     eth.connect();
     lcd.locate(0, 0);
     lcd.printf("IP Address is %s \n", eth.get_ip_address());
     wait_ms(2000);
 }
 
-float getTemperature()
+void setSenderID(PACKET &packet, uint16_t id)
 {
-    uint16_t temperature = temperatureSensor.read();
-    uint16_t temp2 = (temperature << 8) | (temperature >> 8);
-    return temp2 / 256.0;
+    packet.senderID = id;
 }
 
-void setSequenceNumber(int)
+void setSequenceNumber(PACKET &packet)
 {
+    if (sequenceNumber >= 254)
+        sequenceNumber = 0;
+
+    packet.sequenceNumber = sequenceNumber++;
 }
 
-void setPacketOptions()
+void setPacketOptions(PACKET &packet, bool retryFlag, bool ccittFlag, bool ackRequestFlag)
 {
+    uint8_t tempPacketOptions = 0;
+    if (retryFlag)
+        tempPacketOptions += 4;
+    if (ccittFlag)
+        tempPacketOptions += 2;
+    if (ackRequestFlag)
+        tempPacketOptions += 1;
+    lcd.printf("%x", tempPacketOptions);
+    packet.packetOptions = tempPacketOptions;
 }
 
-void setButtonPresses()
+void setButtonPresses(PACKET &packet)
 {
+    uint8_t buttonsPressed = 0;
+    if (sw2Pressed)
+        buttonsPressed += SW2_VAL;
+    if (sw3Pressed)
+        buttonsPressed += SW3_VAL;
+    if (joystickUpPressed)
+        buttonsPressed += JOYSTICK_DOWN_VAL;
+    if (joystickDownPressed)
+        buttonsPressed += JOYSTICK_UP_VAL;
+    if (joystickLeftPressed)
+        buttonsPressed += JOSYTICK_LEFT_VAL;
+    if (joystickFirePressed)
+        buttonsPressed += JOYSTICK_FIRE_VAL;
+
+    packet.buttonPresses = buttonsPressed;
 }
 
-void setTemperature()
+void clearPressedButtons()
 {
+    sw2Pressed = false;
+    sw3Pressed = false;
+    joystickDownPressed = false;
+    joystickUpPressed = false;
+    joystickLeftPressed = false;
+    joystickRightPressed = false;
+    joystickFirePressed = false;
 }
 
-void generateChecksum()
+void setTemperature(PACKET &packet)
 {
+    uint16_t temperatureBigEndian = temperatureSensor.read();
+    packet.temperature = temperatureBigEndian;
 }
 
-void sendPacket(EthernetInterface eth, PACKET &packet)
+void generateChecksum(PACKET &packet)
+{
+    uint8_t packetList[7];
+    packetList[0] = packet.senderID >> 8;
+    packetList[1] = packet.senderID;
+    packetList[2] = packet.sequenceNumber;
+    packetList[3] = packet.packetOptions;
+    packetList[4] = packet.temperature >> 8;
+    packetList[6] = packet.temperature;
+    packetList[7] = packet.buttonPresses;
+
+    uint8_t checksum = (packetList[0] ^ packetList[1] ^ packetList[2] ^ packetList[3] ^ packetList[4] ^ packetList[5] ^ packetList[6] ^ packetList[7]);
+
+    packet.checksum = checksum;
+}
+
+void sendPacket(EthernetInterface eth, uint64_t packet)
 {
     UDPSocket sock(&eth);
     SocketAddress sockAddr;
 
     lcd.cls();
     lcd.locate(0, 0);
-    uint64_t val = 0xdb12010019202000;
-
-    int status = sock.sendto("http://lora.kent.ac.uk", 1789, &val, sizeof(uint64_t));
+    lcd.printf("%llx", packet);
+    int status = sock.sendto("lora.kent.ac.uk", 1789, &packet, sizeof(uint64_t));
     if (0 > status)
     {
         lcd.printf("Error sending data\n %n", status);
     }
     else
     {
-        lcd.printf("sent?");
+        lcd.printf("sent? %n", status);
     }
+    clearPressedButtons();
+}
+
+uint64_t buildPacket(PACKET packet)
+{
+
+    return (((packet.checksum & 0xFFFFFFFFFFFFFFFF) << 56) |
+            ((packet.buttonPresses & 0xFFFFFFFFFFFFFFFF) << 48) |
+            ((packet.temperature & 0xFFFFFFFFFFFFFFFF) << 32) |
+            ((packet.packetOptions & 0xFFFFFFFFFFFFFFFF) << 24) |
+            ((packet.sequenceNumber & 0xFFFFFFFFFFFFFFFF) << 16) |
+            (packet.senderID & 0xFFFFFFFFFFFFFFFF));
 }
